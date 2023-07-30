eb06e32ae686e5dad0b3f910188fd99087339a62
JovanJ <97014515+JOVAN22@users.noreply.github.com>
2023-07-30 17:52:02 +0530

    Update brecon.py

--- brecon.py
+++ brecon.py
@@ -1,13 +1,3 @@
-# for i in range(11):
-#     if(i==5):
-#         break
-#     print("5 X", i + 1, "=", 5 * (i + 1))
-# print("Loop ko chod kar nikal gaya")
-# for i in range(11):
-#     if (i==5):
-#         print("Skip the iteration")
-#         continue
-#     print("5 X",i,"=",5*i)
 
 #ENUMERATE DO WHILE LOOP:
 i=int(input("Enter the number"))
